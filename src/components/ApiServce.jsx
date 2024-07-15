fed4c89e3ac44a6e9d009b3bb169d9069c55ccb6
Juan Giraldo <juanda961003@gmail.com>
2024-07-15 07:15:33 -0500

    se agrega los trabajos aletorios

--- src/components/ApiServce.jsx
+++ src/components/ApiServce.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
 
 
 const ApiServce = ({setPerson}) => {
+
+    const [jobs]= useState([
+        {
+            id:1,
+            job:"Desarrollador de Software"
+        },
+        {
+            id: 2,
+            job: "Ingeniero de Redes"
+        },
+        {
+            id: 3,
+            job: "Analista de Datos"
+        },
+        {
+            id: 4,
+            job: "Especialista en Ciberseguridad"
+        },
+        {
+            id: 5,
+            job: "Administrador de Bases de Datos"
+        },
+        {
+            id: 6,
+            job: "Ingeniero DevOps"
+        },
+        {
+            id: 7,
+            job: "Arquitecto de Soluciones"
+        },
+        {
+            id: 8,
+            job: "Diseñador UX/UI"
+        },
+        {
+            id: 9,
+            job: "Especialista en Inteligencia Artificial"
+        },
+        {
+            id: 10,
+            job: "Gestor de Proyectos de TI"
+        }
+    ]);
+
+    function getRandomJob() {
+        const randomId = Math.floor(Math.random() * 10) + 1;
+        const job = jobs.find(job => job.id === randomId);
+        return job;
+    }
 
     const fetchData =async()=>{
 
@@ -14,7 +63,7 @@ const ApiServce = ({setPerson}) => {
             console.log(data)
             const userData ={
                 name: data.results[0].name.first,
-                role: 'pruebas',
+                role: getRandomJob().job,
                 img: data.results[0].picture.medium
             };
 
